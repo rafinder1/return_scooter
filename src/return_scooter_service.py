22cb2f080bc46406243d7228a45dfcd61bffc41b
rafinder1 <rafpro33@gmail.com>
2023-12-03 20:59:35 +0100

    returned value from method return_scooter

--- src/return_scooter_service.py
+++ src/return_scooter_service.py
@@ -2,11 +2,13 @@ class ReturnScooterService:
     def return_scooter(self, scooter, client, where, minutes):
         price = scooter.price(minutes)
         charged_amount = client.charge(price, scooter.description())
-        if client.is_immediate():
+        if client.get_is_immediate():
             charged_amount = price * 0.9
         self.__charge_client(client.client_id, charged_amount)
-        client.add_loyalty_points(minutes, charged_amount)
-        scooter.schedule_for_maintenance(where)
+        loyalty_points = client.add_loyalty_points(minutes, charged_amount)
+        maintenance = scooter.schedule_for_maintenance(where)
+
+        return charged_amount, maintenance, loyalty_points
 
     @classmethod
     def __charge_client(cls, client_id, charge_amount):
