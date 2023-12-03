a79ccb158402d447437f889b8c30e23da3a98f78
rafinder1 <rafpro33@gmail.com>
2023-12-03 20:16:38 +0100

    created model scooter

---
+++ src/scooter.py
@@ -0,0 +1,5 @@
+class Scooter:
+    def __init__(self, scooter_id, scooter_data, battery_level):
+        self.scooter_id: int = scooter_id
+        self.scooter_data: list = scooter_data
+        self.battery_level: float = battery_level
