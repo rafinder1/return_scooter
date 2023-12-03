6309af8b5247f161a4e2cea7cca1cbbb42a727ca
rafinder1 <rafpro33@gmail.com>
2023-12-03 20:26:18 +0100

    added method in scooter - price, schedule_for_maintenance, description

--- src/scooter.py
+++ src/scooter.py
@@ -1,5 +1,25 @@
+from src.return_scooter_service import Position
+
+
 class Scooter:
     def __init__(self, scooter_id, scooter_data, battery_level):
         self.scooter_id: int = scooter_id
         self.scooter_data: list = scooter_data
         self.battery_level: float = battery_level
+        self.schedule_for_maintenance = None
+
+    def price(self, minutes: int) -> float:
+        unlocking = float(self.scooter_data[1]) if self.scooter_data[0] == "not_fast" else float(
+            self.scooter_data[3])
+        price_per_minute = float(self.scooter_data[2]) if self.scooter_data[
+                                                              0] == "not_fast" else float(
+            self.scooter_data[4])
+        return minutes * price_per_minute + unlocking
+
+    def schedule_for_maintenance(self, where: Position) -> bool:
+        if self.battery_level < 0.07:
+            self.schedule_for_maintenance = True
+        return self.schedule_for_maintenance
+
+    def description(self) -> str:
+        return f"{self.scooter_id} {str(self.scooter_data[0])}"
