78b5f65e43cf317fe8afa59c064b2fbb6b18fdb1
rafinder1 <rafpro33@gmail.com>
2023-12-03 20:21:07 +0100

    added method in client - charge, add_loyalty_points, is_immediate

--- src/client.py
+++ src/client.py
@@ -8,3 +8,25 @@ class Client:
         self.immediate_transactions_counter: int = immediate_transactions_counter
         self.client_price_multiplier: float = client_price_multiplier
         self.monthly_payment_descriptions: list = []
+
+    def charge(self, price: float, description: str) -> float:
+        charge_amount = max(price - self.client_credit, 0)
+        if self.is_immediate:
+            self.immediate_transactions_counter += 1
+        else:
+            self.monthly_payment_descriptions.append(description)
+        return charge_amount
+
+    def add_loyalty_points(self, minutes: int, charge_amount: float) -> int:
+        loyalty_points = 0
+        if 15 < minutes < 50:
+            loyalty_points = 4
+            if self.is_immediate:
+                loyalty_points = 2
+
+        if minutes >= 50 and charge_amount > 30:
+            loyalty_points = 20
+        return loyalty_points
+
+    def is_immediate(self) -> bool:
+        return self.is_immediate
